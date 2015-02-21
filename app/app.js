af5b87f8c058a92f32d4e02401ea7a814fa67aa3
mario cantone <mariocant86@libero.it>
2015-02-21 17:32:24 +0100

    add view 3

--- app/app.js
+++ app/app.js
@@ -5,7 +5,9 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.view3',
   'myApp.version'
+
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
